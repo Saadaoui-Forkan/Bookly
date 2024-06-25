8808b8f33b51df66dfde48c7485671909de8a2d7
unknown <forkan.saadaoui@gmail.com>
2024-06-25 17:06:13 +0100

    prepare to deploy

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,21 @@ app.use('/api/users', require('./routes/userRoute'))
 app.use('/api/books', require('./routes/bookRoute'))
 app.use('/api/favoriteList', require('./routes/favoritesRoute'))
 
+// Prepare For Production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static(path.join(_dirname, 'frontend/build')))
+
+  // any route that is not api will be redirected to index.html
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running ...')
+  })
+}
+
 // running the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
