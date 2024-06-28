c02550dbacb9189ff71f52a8b301091a3e5637cf
unknown <forkan.saadaoui@gmail.com>
2024-06-28 11:59:06 +0100

    update PORT

--- frontend/src/constantes.js
+++ frontend/src/constantes.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:5000/api"
-export const USER_URL = "http://localhost:5000/api/users"
-export const BOOK_URL = "http://localhost:5000/api/books"
-export const FAVORITES_URL = "http://localhost:5000/api/favoriteList"
+export const BASE_URL = "http://localhost:5001/api"
+export const USER_URL = "http://localhost:5001/api/users"
+export const BOOK_URL = "http://localhost:5001/api/books"
+export const FAVORITES_URL = "http://localhost:5001/api/favoriteList"
