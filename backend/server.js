02977a5f910def52171b2fb599ac02947e4976f6
unknown <forkan.saadaoui@gmail.com>
2025-04-22 14:43:33 +0100

    fix deployment bug

--- backend/server.js
+++ backend/server.js
@@ -24,23 +24,6 @@ app.use('/api/users', require('./routes/userRoute'))
 app.use('/api/books', require('./routes/bookRoute'))
 app.use('/api/favoriteList', require('./routes/favoritesRoute'))
 
-// Prepare For Production
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
-}
-
 // running the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () =>
